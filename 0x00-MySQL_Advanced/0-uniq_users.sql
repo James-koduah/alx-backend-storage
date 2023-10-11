70b5c7b316190b3269951e44345348c7b63b40d9
James Koduah <jameskoduah171@gmail.com>
2023-10-11 17:12:46 +0000

    0-uniq_users.sql: first

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Task 0 of MySQL_Advanced
+-- create a user table 
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+	)
