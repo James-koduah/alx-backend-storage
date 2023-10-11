16f507835aa1d9ea8fe26849cad69934830d0b0c
James Koduah <jameskoduah171@gmail.com>
2023-10-11 18:57:39 +0000

    3-glam_rock.sql: first

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,2 @@
+--  script that lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, IFNULL(split, 2022) - formed AS lifespan FROM metal_bands WHERE style LIKE '%glam rock%' ORDER BY lifespan DESC;
