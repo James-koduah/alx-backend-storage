a415ed47f8a8ae2ebfd5cd68fb1eb1bb637920fb
James Koduah <jameskoduah171@gmail.com>
2023-10-11 18:11:42 +0000

    1-country_users.sql: first

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Task 0 of MySQL_Advanced
+-- create a user table 
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+	)
