5a9d54d421f4b69b989d45802f2f51a69a568c76
James Koduah <jameskoduah171@gmail.com>
2023-10-12 17:56:47 +0000

    5-valid_email.sql: added delimeter configuration

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,9 @@
 -- create trigger to update the valid email slot after email changes
+DELIMITER $$
 CREATE TRIGGER check_email AFTER UPDATE ON users FOR EACH ROW
 BEGIN
 	IF NEW.email <> OLD.email
 		SET valid_email = 0
 	END IF;
 END;
+$$
