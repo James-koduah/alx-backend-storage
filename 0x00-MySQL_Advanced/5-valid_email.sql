4933577ef061bcf178e70763adb88ee1c5d07d15
James Koduah <jameskoduah171@gmail.com>
2023-10-12 17:58:35 +0000

    5-valid_email.sql: made trigger before

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,6 +1,6 @@
 -- create trigger to update the valid email slot after email changes
 DELIMITER $$
-CREATE TRIGGER check_email AFTER UPDATE ON users FOR EACH ROW
+CREATE TRIGGER check_email BEFORE UPDATE ON users FOR EACH ROW
 BEGIN
 	IF NEW.email <> OLD.email
 		SET valid_email = 0
