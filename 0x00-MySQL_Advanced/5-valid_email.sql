14e71e644e9e5bc494858d336a644011c39305bc
James Koduah <jameskoduah171@gmail.com>
2023-10-12 17:55:48 +0000

    5-valid_email.sql: first

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,7 @@
+-- create trigger to update the valid email slot after email changes
+CREATE TRIGGER check_email AFTER UPDATE ON users FOR EACH ROW
+BEGIN
+	IF NEW.email <> OLD.email
+		SET valid_email = 0
+	END IF;
+END;
