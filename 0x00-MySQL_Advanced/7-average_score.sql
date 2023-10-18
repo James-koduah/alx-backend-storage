834721a9f33b776f31fad2cb7bf00a85b58ec9b8
James Koduah <jameskoduah171@gmail.com>
2023-10-18 13:32:43 +0000

    7-average_score.sql: first

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,6 @@
+--Compute the average score for a student
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+	UPDATE users
+	SET av_score=(SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id=user_id;
