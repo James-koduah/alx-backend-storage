df2932462535de4d5641611eb7ab3e10203db945
James Koduah <jameskoduah171@gmail.com>
2023-10-18 13:50:05 +0000

    7-average_score.sql: re revert

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,8 +1,11 @@
 -- Compute the average score for a student
 DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 	UPDATE users
-		SET av_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+		SET average_score = (SELECT AVG(score)
+					FROM corrections
+					WHERE corrections.user_id = user_id)
 		WHERE id = user_id;
 END $$
