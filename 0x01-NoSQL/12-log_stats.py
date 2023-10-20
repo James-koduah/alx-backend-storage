1ef9beb6987c2d8679fd9af85c13a1696305e508
James Koduah <jameskoduah171@gmail.com>
2023-10-20 13:14:20 +0000

    12-log_stats.py

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,20 @@
+"""Engage with database of nginx logs"""
+import pymongo
+
+def logs():
+    """Engage with database of nginx logs"""
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    database = client.logs.nginx
+    
+    print("{} logs".format(database.count_documents()))
+    print('\tMethods:')
+    print('\tmethod GET: {}'.format(database.count_documents({'method': 'GET'})))
+    print('\tmethod POST: {}'.format(database.count_documents({'method': 'POST'})))
+    print('\tmethod PUT: {}'.format(database.count_documents({'method': 'PUT'})))
+    print('\tmethod PATCH: {}'.format(database.count_documents({'method': 'PATCH'})))
+    print('\tmethod DELETE: {}'.format(database.count_documents({'method': 'DELETE'})))
+    print("{} status check".format(database.count_documents({'method': 'GET', 'path': '/status'})))
+
+
+if __name__ == '__main__':
+    logs()
