08a67c31cb39b168b54dcfd5bee8670b24537e0c
James Koduah <jameskoduah171@gmail.com>
2023-10-20 12:23:09 +0000

    10-update_topics.py: first

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Update documents in database based on name"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """Update document in database based on name"""
+    return mongo_collection.update_many(
+            {"name": name},
+            {"$set": {'topics': topics}}
+            )
