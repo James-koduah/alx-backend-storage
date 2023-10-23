df310a8d8ca6786196ce0f50067dfa3c00f50171
James Koduah <jameskoduah171@gmail.com>
2023-10-23 22:36:17 +0000

    web.py: second

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,11 +2,13 @@
 """Obtain HTML content from a url and return it"""
 import redis
 import requests
-
+r = redis.Redis()
 
 def get_page(url: str) -> str:
     """get an html page"""
-    r = redis.Redis()
+    cache = r.get(f"cache:{url}")
+    if cache:
+        return cache.decode('utf-8')
     html = requsts.get(url).text
     r.incr(f"count:{url}")
     r.setex(f"cache:{url}", 10, html)
