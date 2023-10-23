c5b7f8cd9099c36e99f2aba7bcf4bdceb9f49dd8
James Koduah <jameskoduah171@gmail.com>
2023-10-23 21:35:28 +0000

    exrcise.py: task 2: first

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,12 +5,22 @@ import uuid
 import typing
 
 
+def count_calls(method: typing.Callable) -> typing.Callable:
+    key = method.__qualname__
+    @wraps(method)
+    def wrap(self, *args, **kwargs):
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrap
+
+
 class Cache():
     """A Cache class using redis"""
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: typing.Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
