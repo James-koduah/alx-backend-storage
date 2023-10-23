3789a473d400d9c3d8079597b634b5cb2753adc5
James Koduah <jameskoduah171@gmail.com>
2023-10-23 22:46:18 +0000

    web.py: fourth

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,14 +2,25 @@
 """Obtain HTML content from a url and return it"""
 import redis
 import requests
+from functools import wraps
 r = redis.Redis()
 
+
+def cache_get_page(func):
+    @wraps(func)
+    def wrap(url: str):
+        old_cache = r.get(f"cache:{url}")
+        if old_cache:
+            return old_cache.decode("utf-8")
+
+        new_cache = func(url)
+        r.setex(f'cache:{url}', 10, new_cache)
+        return new_cache
+    return wrap
+
+@cache_get_page
 def get_page(url: str) -> str:
     """get an html page"""
-    cache = r.get(f"cache:{url}")
-    if cache:
-        return cache.decode('utf-8')
     r.incr(f"count:{url}")
-    html = requsts.get(url).text
-    r.setex(f"cache:{url}", 10, html)
+    html = requests.get(url).text
     return html
