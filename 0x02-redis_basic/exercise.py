de7166c7ac478a719a49149d91c3feaa631e7575
James Koduah <jameskoduah171@gmail.com>
2023-10-23 19:50:32 +0000

    exercise.py: task 1: first

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,3 +15,17 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key, fn):
+        value = self._redis.get(key)
+        if fn is not None:
+            value = fn(value)
+        return value
+
+    def get_str(self, key):
+        value = self._redis.get(key)
+        return str(value)
+
+    def get_int(self, key):
+        value = self._redis.get(key)
+        return int(value)
