040f5983a9d097f69496f6ebdf6b93a34e1b9269
James Koduah <jameskoduah171@gmail.com>
2023-10-24 22:09:29 +0000

    web.py: sixth

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,16 +7,16 @@ from functools import wraps
 r = redis.Redis()
 
 
-def cache_get_page(func):
+def cache_get_page(funct):
     """wrapper function for get_page function"""
 
-    @wraps(func)
+    @wraps(funct)
     def wrap(url: str):
         old_cache = r.get(f"cache:{url}")
         if old_cache:
             return old_cache.decode("utf-8")
 
-        new_cache = func(url)
+        new_cache = funct(url)
         r.setex(f'cache:{url}', 10, new_cache)
         return new_cache
 
